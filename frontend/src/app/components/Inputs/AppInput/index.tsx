b6e03c463b3a32de099e2e082d1f1844c3f67f50
Lucasvalves <lucasvalves98@gmail.com>
2025-04-12 11:27:26 -0300

    feat: update edit-profile route

--- frontend/src/app/components/Inputs/AppInput/index.tsx
+++ frontend/src/app/components/Inputs/AppInput/index.tsx
@@ -2,26 +2,24 @@
 import style from './page.module.scss'
 
 type Props = {
-  placeholder:string
-  type:string
-  label:string
-
+  placeholder?:string
+  type?:string
+  label?:string
+  className?:string
 }
-export default function AppInput({label, placeholder}:Props){
+export default function AppInput({label, placeholder, type, className}:Props){
 
   return(
       <div className={style.container}>
         <label htmlFor="fullName">{label}</label>
-        <div className={style.inputContainer}>
+        <div className={`${style.inputContainer} ${className}`}>
           <input
             id="email"
-            type="email"
+            type={type ? type : 'text'}
             placeholder={placeholder}
-            className="input"
             required
             />
         </div>
       </div>
-         
   )
 }
--- frontend/src/app/edit-profile/page.tsx
+++ frontend/src/app/edit-profile/page.tsx
@@ -1,12 +1,32 @@
+import Form from "next/form";
+import { ButtonGroup } from "../components/ButtonGroup";
+import AppInput from "../components/Inputs/AppInput";
 import { PageLayout } from "../components/PageLayout";
 import styles from './page.module.scss'
+import Image from "next/image";
+import { MdOutlineEdit } from "react-icons/md";
 
 export default function EditProfile(){
   return(
     <div className={styles.page}>
       <PageLayout title="Editar Perfil" >
-        <div>
-          
+        <div className={styles.formWrapper}>
+          <Form action="">
+            <div className={styles.itemsCenter}>
+            <label className={styles.avatarWrapper}>
+              <Image src="/default-profile.png" width={100} height={100} alt="Avatar" className={styles.avatarImage} />
+              <input aria-label="edit" type="file" className={styles.fileInput} />
+              <div className={styles.editIcon}>
+                <MdOutlineEdit size='20'/>
+              </div>
+            </label>
+            <AppInput label="Nome" placeholder="Luiza Gomes" className={styles.containerInput}/>
+            <AppInput label="Email" placeholder="luizagomes.t@gmail.com" className={styles.containerInput}/>
+            <AppInput label="Nova senha" placeholder="******" type="password" className={styles.containerInput}/>
+            <AppInput label="Confirmar senha" placeholder="******" type="password" className={styles.containerInput}/>
+            <ButtonGroup rightButtonLabel="Cancelar" leftButtonLabel="Editar" />
+          </div>
+          </Form>
         </div>
       </PageLayout>
     </div>
