b63bf4d223626379f28820a68b3c268946d46228
Lucasvalves <lucasvalves98@gmail.com>
2025-05-22 09:24:47 -0300

    feat: updating input file

--- frontend/src/app/edit-profile/page.tsx
+++ frontend/src/app/edit-profile/page.tsx
@@ -1,3 +1,5 @@
+
+"use client"
 import Form from "next/form";
 import { ButtonGroup } from "../components/ButtonGroup";
 import AppInput from "../components/Inputs/AppInput";
@@ -5,8 +7,31 @@ import { PageLayout } from "../components/PageLayout";
 import styles from './page.module.scss'
 import Image from "next/image";
 import { MdOutlineEdit } from "react-icons/md";
+import { useState, useRef} from "react";
 
 export default function EditProfile(){
+  const [imageUrl, setImageUrl] = useState<string | null >(null)
+  const fileInputRef = useRef<HTMLInputElement>(null)
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0]
+    if (file) {
+
+      if (!file.type.startsWith('image/')) {
+        alert('Por favor, selecione um arquivo de imagem');
+        return;
+      }
+      
+      const objectUrl = URL.createObjectURL(file);
+      setImageUrl(objectUrl);
+
+      return () => URL.revokeObjectURL(objectUrl);
+    }
+  };
+
+  const triggerFileInput = () => {
+    fileInputRef.current?.click();
+  };
   return(
     <div className={styles.page}>
       <PageLayout title="Editar Perfil" >
@@ -14,8 +39,8 @@ export default function EditProfile(){
           <Form action="">
             <div className={styles.itemsCenter}>
             <label className={styles.avatarWrapper}>
-              <Image src="/default-profile.png" width={100} height={100} alt="Avatar" className={styles.avatarImage} />
-              <input aria-label="edit" type="file" className={styles.fileInput} />
+              <Image src={imageUrl || "/default-profile.png"} width={100} height={100} alt="Avatar" className={styles.avatarImage} onClick={triggerFileInput}/>
+              <input aria-label="edit" type="file"  accept="image/*" className={styles.fileInput} onChange={handleFileChange}/>
               <div className={styles.editIcon}>
                 <MdOutlineEdit size='20'/>
               </div>
