46c5d07568311236b3a4781eaef83188a0e06d90
Lucasvalves <lucasvalves98@gmail.com>
2025-08-26 10:33:14 -0300

    feat: add HTTP client implementation

---
+++ frontend/src/infrastructure/http/http-client.ts
@@ -0,0 +1,33 @@
+import axios, { AxiosError, AxiosInstance } from "axios";
+import { IHttpClient, HttpRequest} from "../contratcs/http-contratcs";
+
+export class HttpClient implements IHttpClient {
+
+    constructor(private api: AxiosInstance) {}
+
+    static create(): HttpClient {
+        return new HttpClient(axios)
+    }
+
+    async sendRequest<TResponse, TBody = unknown>(props: HttpRequest<TBody>): Promise<TResponse> {
+        const { endpoint, method, body, headers, params } = props;
+
+        try {
+            const data = await this.api.request<TResponse>({
+                method,
+                headers,
+                data: body,
+                url: `${process.env.NEXT_PUBLIC_API_BETTER_HEALTH_URL}${endpoint}`,
+                params
+            })
+
+            return data.data;
+        } catch (err) {
+            const error = err as AxiosError;
+            const status = error.response?.status || 500;
+            const message = error.response?.data || error.message;
+			throw new Error(`Request failed with status ${status}: ${message}`)
+
+        }
+    }
+}
