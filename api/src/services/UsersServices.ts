16aa7d832b469ad526611ab21255449b033b0db2
Lucasvalves <lucasvalves98@gmail.com>
2025-10-31 11:05:52 -0300

    refactor: improve user update logic

--- api/src/services/UsersServices.ts
+++ api/src/services/UsersServices.ts
@@ -3,7 +3,7 @@ import { ICreate, IUpdate } from '../interfaces/UserInterface'
 import { UsersRepository } from '../repositories/UsersRepository'
 import { sign } from 'jsonwebtoken'
 import { s3 } from '../config/aws'
-import { v4 as uuid } from "uuid"
+import { v4 as uuid } from 'uuid'
 
 class UsersServices {
 	private usersRepository: UsersRepository
@@ -11,7 +11,6 @@ class UsersServices {
 		this.usersRepository = new UsersRepository()
 	}
 	async create({ name, email, password }: ICreate) {
-
 		const hashPassword = await hash(password, 10)
 
 		const create = await this.usersRepository.create({
@@ -21,43 +20,6 @@ class UsersServices {
 		})
 		return create
 	}
-	async update({
-    oldPassword,
-    newPassword,
-    avatar_url,
-    user_id,
-  }: IUpdate) {
-    let password;
-    if (oldPassword && newPassword) {
-      const findUserById = await this.usersRepository.findUserById(user_id);
-      if (!findUserById) {
-        throw new Error('User not found');
-      }
-      const passwordMatch = compare(oldPassword, findUserById.password);
-
-      if (!passwordMatch) {
-        throw new Error('Password invalid.');
-      }
-      password = await hash(newPassword, 10);
-
-      await this.usersRepository.updatePassword(newPassword, user_id);
-    }
-    if (avatar_url) {
-      const uploadImage = avatar_url?.buffer;
-      const uploadS3 = await s3
-        .upload({
-          Bucket: 'better-health',
-          Key: `${uuid()}-${avatar_url?.originalname}`,
-          Body: uploadImage,
-        })
-        .promise();
-
-      await this.usersRepository.update( uploadS3.Location, user_id);
-    }
-    return {
-      message: 'User updated successfully',
-    };
-  }
 	async auth(email: string, password: string) {
 		const findUser = await this.usersRepository.findUserByEmail(email)
 		if (!findUser) {
@@ -86,6 +48,44 @@ class UsersServices {
 				name: findUser.name,
 				email: findUser.email,
 			},
+		}
+	}
+	async update({ oldPassword, newPassword, avatar_url, user_id }: IUpdate) {
+		let password
+		if (oldPassword && newPassword) {
+			const findUserById = await this.usersRepository.findUserById(user_id)
+			if (!findUserById) {
+				throw new Error('User not found')
+			}
+			if (oldPassword === newPassword) {
+				throw new Error('Both passwords are the same')
+			}
+			const passwordMatch = compare(oldPassword, findUserById.password)
+
+			if (!passwordMatch) {
+				throw new Error('Password invalid.')
+			}
+			password = await hash(newPassword, 10)
+
+			await this.usersRepository.updatePassword(newPassword, user_id)
+			return {
+				message: 'User updated successfully',
+			}
+		}
+		if (avatar_url) {
+			const uploadImage = avatar_url?.buffer
+			const uploadS3 = await s3
+				.upload({
+					Bucket: 'better-health',
+					Key: `${uuid()}-${avatar_url?.originalname}`,
+					Body: uploadImage,
+				})
+				.promise()
+
+			await this.usersRepository.update(uploadS3.Location, user_id)
+			return {
+				message: 'User updated successfully',
+			}
 		}
 	}
 }
