5f388d08cf46aec87a1b0ebadb31eb0366b10757
Lucasvalves <lucasvalves98@gmail.com>
2025-11-21 18:00:00 -0300

    feat: update navigation and introduce profile dropdown

--- frontend/src/app/(protect)/components/Header/Nav/index.tsx
+++ frontend/src/app/(protect)/components/Header/Nav/index.tsx
@@ -1,25 +1,67 @@
 'use client'
 
 import { enumsRoutes } from '@/shared/enums/enumsRoutes'
-import { ItemNavegation } from './ItemNavegation'
 import styles from './page.module.scss'
 import { useNavModel } from './nav-model'
+import Link from 'next/link'
+import { CgProfile } from 'react-icons/cg'
+import { useEffect, useRef, useState } from 'react'
+import { FiLogOut } from 'react-icons/fi'
 
 export const Nav = () => {
   const { logout } = useNavModel()
+  const [open, setOpen] = useState(false)
+
+  const profileRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        profileRef.current &&
+        !profileRef.current.contains(event.target as Node)
+      ) {
+        setOpen(false)
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => document.removeEventListener('mousedown', handleClickOutside)
+  }, [])
+
   return (
     <div className={styles.container}>
-      <ItemNavegation to={enumsRoutes.APPOINTMENTS}>
-        Realizar Agendamento
-      </ItemNavegation>
-      <ItemNavegation to={enumsRoutes.SEARCH_APPOINTMENTS}>
-        Buscar Agendamentos
-      </ItemNavegation>
-      <ItemNavegation to={enumsRoutes.SCHEDULE}>Montar Agenda</ItemNavegation>
-      <ItemNavegation to={enumsRoutes.PROFILE}>Editar Perfil</ItemNavegation>
-      <ItemNavegation onClick={logout} to={enumsRoutes.AUTHENTICATION}>
-        Sair
-      </ItemNavegation>
+      <Link href={enumsRoutes.APPOINTMENTS}>Realizar Agendamento</Link>
+      <Link href={enumsRoutes.SEARCH_APPOINTMENTS}>Buscar Agendamentos</Link>
+      <Link href={enumsRoutes.SCHEDULE}>Montar Agenda</Link>
+      <div className={styles.profile} ref={profileRef}>
+        <div className={styles.dropdown} onClick={() => setOpen(!open)}>
+          <CgProfile size={18} />
+          <span>Perfil</span>
+
+          <ul className={`${styles.dropdownMenu} ${open && styles.open}`}>
+            <Link href={enumsRoutes.REGISTER_DOCTOR}>
+              <li className={styles.dropdownMenuItem}>Cadastrar Médico</li>
+            </Link>
+            <Link href={enumsRoutes.REGISTER_PATIENT}>
+              <li className={styles.dropdownMenuItem}>Cadastrar Paciente</li>
+            </Link>
+            <Link href={enumsRoutes.REGISTER_SPECIALTY}>
+              <li className={styles.dropdownMenuItem}>
+                Cadastrar Especialidade
+              </li>
+            </Link>
+            <Link href={enumsRoutes.PROFILE}>
+              <li className={styles.dropdownMenuItem}>Editar Perfil</li>
+            </Link>
+            <Link href={enumsRoutes.AUTHENTICATION} onClick={logout}>
+              <li className={styles.logout}>
+                <FiLogOut className={styles.icon} />
+                Sair
+              </li>
+            </Link>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
